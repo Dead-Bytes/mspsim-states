5d40ee266e6f1e3ee5fb5318f8b6e6ac8d555801
Dead-Bytes <divyraj.saini22b@iiitg.ac.in>
2025-04-10 15:57:13 +0530

    "ours implemented"

--- se/sics/mspsim/core/MSP430.java
+++ se/sics/mspsim/core/MSP430.java
@@ -147,7 +147,12 @@ public class MSP430 extends MSP430Core {
                 double cpuPercent = getCPUPercent();
                 if (cpuPercent > 50) {
                     System.out.println("Battery sufficient: " + cpuPercent + "%");
-                } else if (cpuPercent < 50 ) {
+                } else if (cpuPercent < 50 && cpuPercent > 20 ) {
+                    if (batch_cycle > 20000){
+                      System.out.println("Batch checkpointing limit reached.");
+                      saveStateToFlash();
+                    }
+                } else if (cpuPercent < 20) {
                     System.out.println("Saving state to flash: " + cpuPercent + "%");
                     saveStateToFlash();
 
